71015b6e9401a62e866a53b908f7ecff7f0946d2
AmanChandra1c <amanchandr1c7662@gmail.com>
2025-03-03 23:05:19 +0530

    Database Setup

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,9 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const database = require('./DB/db');
 
+database();
 app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello, World!');
